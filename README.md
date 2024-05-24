abb37cf458840d9d3379e629d830b312b4df1e20
Huyxu2k <nguyenvanhuy2712000@gmail.com>
2024-05-24 07:49:37 +0700

    init Structure of project

---
+++ README.md
@@ -0,0 +1,1 @@
+## Structure of AUTH-RUST is cloned from  [auth-rs](https://github.com/Opserva-io/auth-rs/tree/master)
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod components;
+pub mod configuration;
+pub mod errors;
+pub mod repository;
+pub mod services;
+pub mod web;
